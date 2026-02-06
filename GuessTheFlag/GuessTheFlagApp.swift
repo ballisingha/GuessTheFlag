2c608fbb6ba4b8b3ddf1a0e7f5e311bbd72d0846
ballisingha <guriqbal@hotmail.de>
2026-02-06 23:11:43 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Guriqbal Singh Amroke on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
